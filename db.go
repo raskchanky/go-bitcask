54cc7aac0f14e5f66b80e49ae9bd08513e90eeb2
Josh Black <raskchanky@gmail.com>
2022-07-26 17:47:55 -0700

    add stable file compaction

--- db.go
+++ db.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"hash/crc64"
+	"io"
 	"os"
 	"path/filepath"
 	"sort"
@@ -30,18 +31,20 @@ var (
 )
 
 type DB struct {
-	baseDir    string
-	activeFile *os.File
-	data       map[string]*KeyDir
-	m          sync.RWMutex
-	offset     *atomic.Int64
+	baseDir           string
+	activeFile        *os.File
+	data              map[string]*KeyDir
+	m                 sync.RWMutex
+	offset            *atomic.Int64
+	compactionRunning *atomic.Bool
 }
 
 // Open creates a new database at the given path and returns a handle to it along with any errors.
 func Open(baseDir string) (*DB, error) {
 	d := &DB{
-		baseDir: baseDir,
-		offset:  atomic.NewInt64(0),
+		baseDir:           baseDir,
+		offset:            atomic.NewInt64(0),
+		compactionRunning: atomic.NewBool(false),
 	}
 
 	err := os.MkdirAll(baseDir, 0o600)
@@ -62,55 +65,26 @@ func Open(baseDir string) (*DB, error) {
 }
 
 func (d *DB) Get(key string) ([]byte, error) {
+	// look up the keydir associated with this key. if it's not there, we don't have it.
 	d.m.RLock()
 	kd, ok := d.data[key]
 	d.m.RUnlock()
 
-	// look up the keydir associated with this key. if it's not there, we don't have it.
 	if !ok {
 		return nil, ErrNotFound
 	}
 
 	// kd.Offset tells us where to start reading. The first thing we'll read is the size,
 	// in a fixed 4 byte buffer.
-	offset := kd.Offset
-	buf := make([]byte, sizeBufferSize)
 	f, err := os.Open(kd.FileId)
 	if err != nil {
 		return nil, err
 	}
 	defer func() { _ = f.Close() }()
 
-	num, err := f.ReadAt(buf, offset)
+	entry, _, err := readProtoFromFile(f, kd.Offset)
 	if err != nil {
 		return nil, err
-	}
-
-	offset += int64(num)
-	readSize := binary.LittleEndian.Uint32(buf)
-	msg := make([]byte, readSize)
-
-	// Now that we know how big the proto is, read that out as well
-	_, err = f.ReadAt(msg, offset)
-	if err != nil {
-		return nil, err
-	}
-
-	var entry Entry
-	err = proto.Unmarshal(msg, &entry)
-	if err != nil {
-		return nil, err
-	}
-
-	ed, err := proto.Marshal(entry.EntryData)
-	if err != nil {
-		return nil, err
-	}
-
-	// check the crc
-	crc := entryDataChecksum(ed)
-	if crc != entry.Crc {
-		return nil, ErrChecksumMismatch
 	}
 
 	return copyBytes(entry.EntryData.Value), nil
@@ -121,41 +95,19 @@ func (d *DB) Put(key string, val []byte) error {
 	defer d.m.Unlock()
 
 	startingOffset := d.offset.Load()
-
-	// construct a new entry using the key/val
-	entry, err := d.makeEntry(key, val, bytes.Equal(tombstoneValue, val))
+	entry, err := makeEntry(key, val, bytes.Equal(tombstoneValue, val))
 	if err != nil {
 		return err
 	}
 
-	// append to the active file, writing the size first
-	data, err := proto.Marshal(entry)
-	if err != nil {
-		return err
-	}
-
-	buf := make([]byte, sizeBufferSize)
-	binary.LittleEndian.PutUint32(buf, uint32(len(data)))
-
-	num, err := d.activeFile.WriteAt(buf, startingOffset)
-	if err != nil {
-		return err
-	}
-
-	// Now that the size is written, append the encoded protobuf
-	currentOffset := startingOffset + int64(num)
-	num, err = d.activeFile.WriteAt(data, currentOffset)
-	if err != nil {
-		return err
-	}
-	currentOffset += int64(num)
+	currentOffset, err := writeProtoToFile(d.activeFile, entry, startingOffset)
 	d.offset.Store(currentOffset)
 
 	// update in memory map, using the starting instead of ending offset
 	d.data[key] = &KeyDir{
 		FileId:    d.activeFile.Name(),
 		Offset:    startingOffset,
-		Timestamp: time.Now().UnixNano(),
+		Timestamp: entry.EntryData.Timestamp,
 	}
 
 	return d.activeFile.Sync()
@@ -197,7 +149,7 @@ func (d *DB) Path() string {
 	return d.baseDir
 }
 
-func (d *DB) makeEntry(key string, val []byte, tombstone bool) (*Entry, error) {
+func makeEntry(key string, val []byte, tombstone bool) (*Entry, error) {
 	ed := &EntryData{
 		Timestamp: time.Now().UnixNano(),
 		Key:       key,
@@ -227,7 +179,7 @@ func (d *DB) fileRotation() {
 		}
 
 		if fi.Size() < fileSizeThreshold {
-			time.Sleep(10 * time.Millisecond)
+			time.Sleep(200 * time.Millisecond)
 			continue
 		}
 
@@ -259,38 +211,110 @@ func (d *DB) fileRotation() {
 		}
 		d.m.Unlock()
 
-		go d.compactStableFiles()
+		if !d.compactionRunning.Load() {
+			go d.compactStableFiles()
+		}
 	}
 }
 
 func (d *DB) compactStableFiles() {
-	// d.m.RLock()
-	// baseDir := d.baseDir
-	// d.m.RUnlock()
-	//
-	// f, err := os.Open(baseDir)
-	// if err != nil {
-	// 	// TODO: handle this more gracefully
-	// 	panic(err)
-	// }
-	//
-	// dirEntries, err := f.ReadDir(0)
-	// if err != nil {
-	// 	// TODO: handle this more gracefully
-	// 	panic(err)
-	// }
-	//
-	// for _, de := range dirEntries {
-	// 	if !strings.HasSuffix(de.Name(), ".stable") {
-	// 		continue
-	// 	}
-	//
-	// 	sf, err := os.Open(de.Name())
-	// 	if err != nil {
-	// 		// TODO: handle this more gracefully
-	// 		panic(err)
-	// 	}
-	// }
+	d.compactionRunning.Store(true)
+	defer d.compactionRunning.Store(false)
+
+	d.m.RLock()
+	baseDir := d.baseDir
+	d.m.RUnlock()
+
+	toDelete := make(map[string]struct{}, 0)
+	latestVersions := make(map[string]*Entry)
+
+	f, err := os.Open(baseDir)
+	if err != nil {
+		// TODO: handle this more gracefully
+		panic(err)
+	}
+
+	dirEntries, err := f.ReadDir(0)
+	if err != nil {
+		// TODO: handle this more gracefully
+		panic(err)
+	}
+
+	for _, de := range dirEntries {
+		if filepath.Ext(de.Name()) != "stable" {
+			continue
+		}
+
+		sf, err := os.Open(de.Name())
+		if err != nil {
+			// TODO: handle this more gracefully
+			panic(err)
+		}
+
+		for {
+			var sfOffset int64
+			entry, sfOffset, err := readProtoFromFile(sf, sfOffset)
+			if err == io.EOF {
+				break
+			}
+			key := entry.EntryData.Key
+
+			d.m.RLock()
+			_, ok := d.data[key]
+			d.m.RUnlock()
+
+			if !ok {
+				continue
+			}
+
+			lv, ok := latestVersions[key]
+			if !ok || entry.EntryData.Timestamp > lv.EntryData.Timestamp {
+				latestVersions[key] = entry
+			}
+		}
+
+		toDelete[de.Name()] = struct{}{}
+	}
+
+	var currentSize, startingOffset, endingOffset int64
+	var currentFile *os.File
+
+	for _, entry := range latestVersions {
+		if currentSize == 0 {
+			newFileName := fmt.Sprintf("%d.stable", time.Now().UTC().UnixNano())
+			newFullPath := filepath.Join(baseDir, newFileName)
+			currentFile, err = os.OpenFile(newFullPath, os.O_RDWR|os.O_CREATE, 0o600)
+			if err != nil {
+				// TODO: handle this more gracefully
+				panic(err)
+			}
+		}
+
+		endingOffset, err = writeProtoToFile(currentFile, entry, startingOffset)
+		if err != nil {
+			// TODO: handle this more gracefully
+			panic(err)
+		}
+		currentSize += endingOffset - startingOffset
+		startingOffset = endingOffset
+
+		if currentSize >= fileSizeThreshold {
+			err = currentFile.Close()
+			if err != nil {
+				// TODO: handle this more gracefully
+				panic(err)
+			}
+			currentSize = 0
+		}
+	}
+
+	for k := range toDelete {
+		err := os.Remove(k)
+		if err != nil {
+			// TODO: handle this more gracefully
+			panic(err)
+		}
+	}
 }
 
 // this should be called either at startup or with the lock held
@@ -305,6 +329,70 @@ func (d *DB) newActiveFile() error {
 	d.activeFile = f
 	d.offset.Store(0)
 	return nil
+}
+
+func writeProtoToFile(f *os.File, entry *Entry, offset int64) (int64, error) {
+	// append to the active file, writing the size first
+	data, err := proto.Marshal(entry)
+	if err != nil {
+		return 0, err
+	}
+
+	buf := make([]byte, sizeBufferSize)
+	binary.LittleEndian.PutUint32(buf, uint32(len(data)))
+
+	num, err := f.WriteAt(buf, offset)
+	if err != nil {
+		return 0, err
+	}
+
+	// Now that the size is written, append the encoded protobuf
+	currentOffset := offset + int64(num)
+	num, err = f.WriteAt(data, currentOffset)
+	if err != nil {
+		return 0, err
+	}
+
+	currentOffset += int64(num)
+	return currentOffset, nil
+}
+
+func readProtoFromFile(f *os.File, offset int64) (*Entry, int64, error) {
+	buf := make([]byte, sizeBufferSize)
+	num, err := f.ReadAt(buf, offset)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	offset += int64(num)
+	readSize := binary.LittleEndian.Uint32(buf)
+	msg := make([]byte, readSize)
+
+	// Now that we know how big the proto is, read that out as well
+	num, err = f.ReadAt(msg, offset)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	offset += int64(num)
+	var entry Entry
+	err = proto.Unmarshal(msg, &entry)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	ed, err := proto.Marshal(entry.EntryData)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// check the crc
+	crc := entryDataChecksum(ed)
+	if crc != entry.Crc {
+		return nil, 0, ErrChecksumMismatch
+	}
+
+	return &entry, offset, nil
 }
 
 func copyBytes(val []byte) []byte {
